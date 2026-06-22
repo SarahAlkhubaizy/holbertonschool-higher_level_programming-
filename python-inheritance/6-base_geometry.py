7d00a2d2b1b8d6335cba12e53e583ccae6383ceb
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-06-22 22:23:54 +0300

    Add file

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Defines class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Method not implemented yet."""
+        raise Exception("area() is not implemented")
