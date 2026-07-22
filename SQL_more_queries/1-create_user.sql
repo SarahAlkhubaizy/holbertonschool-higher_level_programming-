55692876868e34aa826470e83ccec2c01136f02c
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-07-22 17:30:20 +0300

    Edit file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- creates the user user_0d_1 if it does not already existCREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
--- give grants to user-- grants all privileges on all databases to user_0d_1
-GRANT ALL PRIVILEGES 
-ON *.*
-TO 'user_0d_1'@'localhost';
+-- create user and give privlges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
